e11754aa65cb9fddc6947f4689c0f97008f49755
ranjanbipul257 <78407413+ranjanbipul257@users.noreply.github.com>
2024-01-25 15:21:16 +0530

    Add content

--- article/views.py
+++ article/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from . models import *
 # Create your views here.
 
 def index(request):
-    return render(request,'article/index.html')
+    all_title=Article.objects.all()
+    context={'title':all_title,}
+    return render(request,'article/index.html',context)
